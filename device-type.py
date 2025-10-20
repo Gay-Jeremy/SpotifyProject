cbb6f5e946a7b3190fb4f7a1265bba0d60d52cdf
Gay-Jeremy <jeremy.gay.dev@gmail.com>
2025-10-20 11:49:21 +0200

    ajout exercice 5 & 6

---
+++ device-type.py
@@ -0,0 +1,18 @@
+from spotify import dataset
+lstPaysDevice = {}
+for record in dataset :
+    country = record["country"]
+    deviceType = record["device_type"]
+    if country not in lstPaysDevice :
+        lstPaysDevice[country] = {deviceType : 1}
+    if country in lstPaysDevice and deviceType not in lstPaysDevice[country] :
+        lstPaysDevice[country][deviceType] = 1
+    if country in lstPaysDevice and deviceType in lstPaysDevice[country] :
+        lstPaysDevice[country][deviceType] += 1
+
+for pays, device in lstPaysDevice.items() :
+        for dev, val in device.items() :
+            maxDevice = max(device.values())
+        print(f"Pour le pays {pays} le device le plus utilisé est {dev}")
+
+
